fd5354771511f843a42b10007a831d1ca5b4b367
Andres Villegas <andres.villegas@hotschedules.com>
2020-03-25 18:50:24 -0500

    fix edit bug2

--- src/components/world/edit-world.component.js
+++ src/components/world/edit-world.component.js
@@ -74,7 +74,7 @@ export default class EditExercise extends Component {
 		console.log(exercise);
 
 		axios
-			.post('http://localhost:5000/exercises/update/' + this.props.match.params.id, exercise)
+			.post('http://localhost:5050/exercises/update/' + this.props.match.params.id, exercise)
 			.then(res => console.log(res.data));
 
 		window.location = '/worlds';
